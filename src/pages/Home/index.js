40cd6f19373b72f857f1c5ff51c8af05c685836e
brendamatias <brendamatias.sobral@gmail.com>
2020-05-16 00:24:15 -0300

    update home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,21 +4,44 @@ import Header from '~/components/Header';
 import Button from '~/components/Button';
 
 import logoBlack from '~/assets/logo-black.png';
+import teste from '~/assets/image-teste.jpg';
 
-import { Container } from './styles';
+import { Banner, Products } from './styles';
 
 export default function Home() {
   return (
     <>
       <Header />
-      <Container>
+      <Banner>
         <div>
           <img src={logoBlack} alt="Biscoitxs" />
           <h1>bom para você, bom para todxs.</h1>
 
           <Button>Vem cá e conheçe a gente</Button>
         </div>
-      </Container>
+      </Banner>
+
+      <Products>
+        <h1>Ei, aproveita as novidades!</h1>
+
+        <ul>
+          <li>
+            <div>
+              <img src={teste} alt="Biscoitxs" />
+            </div>
+          </li>
+          <li>
+            <div>
+              <img src={teste} alt="Biscoitxs" />
+            </div>
+          </li>
+          <li>
+            <div>
+              <img src={teste} alt="Biscoitxs" />
+            </div>
+          </li>
+        </ul>
+      </Products>
     </>
   );
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 import background from '~/assets/background-home.png';
 
-export const Container = styled.div`
+export const Banner = styled.div`
   background: url(${background}) no-repeat center;
   background-color: #fff;
   background-size: cover;
@@ -29,6 +29,39 @@ export const Container = styled.div`
     button {
       margin-top: 30px;
       width: 300px;
+    }
+  }
+`;
+
+export const Products = styled.div`
+  background: #69d8e1;
+
+  h1 {
+    font-size: 30px;
+    color: #000;
+    font-weight: 800;
+  }
+
+  ul {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-gap: 50px;
+
+    li {
+      div {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        width: 220px;
+
+        background: #fff;
+
+        img {
+          width: 180px;
+          height: 240px;
+        }
+      }
     }
   }
 `;
