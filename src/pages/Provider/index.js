faacfb939210789e67ae7613164998739b5e388f
brendamatias <brendamatias.sobral@gmail.com>
2020-05-17 01:24:59 -0300

    provider page

--- src/pages/Provider/index.js
+++ src/pages/Provider/index.js
@@ -1,7 +1,74 @@
 import React from 'react';
+import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
+import { TiStarFullOutline } from 'react-icons/ti';
 
-function Provider() {
-  return <div />;
+import Header from '~/components/HeaderPrimary';
+import Footer from '~/components/Footer';
+
+import provider from '~/assets/provider.png';
+import product from '~/assets/product.png';
+
+import { Container } from './styles';
+
+export default function Provider() {
+  return (
+    <>
+      <Header />
+
+      <Container>
+        <div className="container">
+          <div className="flex">
+            <img src={provider} alt="Ciclano" />
+
+            <div>
+              <h1>Ciclano Beltrano</h1>
+
+              <strong>Sobre</strong>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries,
+              </p>
+
+              <div className="flex bottom">
+                <div>
+                  <strong>Avaliações</strong>
+
+                  <p>
+                    <TiStarFullOutline color="#FFC107" size={30} />
+                    <TiStarFullOutline color="#FFC107" size={30} />
+                    <TiStarFullOutline color="#FFC107" size={30} />
+                    <TiStarFullOutline color="#FFC107" size={30} />
+                    <TiStarFullOutline color="#FFC107" size={30} />
+                  </p>
+                </div>
+                <div>
+                  <strong>Redes sociais</strong>
+                  <div>
+                    <FaFacebook color="#01C57D" size={30} />
+                    <FaTwitter color="#01C57D" size={30} />
+                    <FaInstagram color="#01C57D" size={30} />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="products">
+            <strong>Anúncios do vendedxr</strong>
+
+            <div>
+              <img src={product} alt="Product" />
+              <img src={product} alt="Product" />
+              <img src={product} alt="Product" />
+              <img src={product} alt="Product" />
+            </div>
+          </div>
+        </div>
+      </Container>
+      <Footer />
+    </>
+  );
 }
-
-export default Provider;
---
+++ src/pages/Provider/styles.js
@@ -0,0 +1,71 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background: #fff;
+  padding: 80px 0;
+
+  img {
+    margin-right: 80px;
+  }
+
+  .flex {
+    display: flex;
+  }
+
+  > div > div {
+    align-items: center;
+  }
+
+  h1 {
+    font-weight: 700;
+    font-size: 34px;
+    color: #ff6494;
+  }
+
+  p {
+    font-size: 20px;
+    max-width: 500px;
+    margin: 30px 0;
+    font-weight: 600;
+  }
+
+  strong {
+    font-weight: 800;
+    font-size: 30px;
+    margin: 20px 0;
+    display: block;
+  }
+
+  .bottom {
+    strong,
+    p {
+      margin: 0;
+    }
+
+    div {
+      margin-right: 60px;
+    }
+
+    > div > div {
+      svg {
+        margin-right: 20px;
+      }
+    }
+  }
+
+  .products {
+    margin-top: 60px;
+
+    div {
+      img {
+        max-width: 220px;
+        box-shadow: 9px 9px 9px rgba(0, 0, 0, 0.15);
+      }
+
+      margin-top: 60px;
+      display: grid;
+      grid-template-columns: repeat(auto-fit, minmax(180px, 1fr));
+      grid-gap: 40px;
+    }
+  }
+`;
